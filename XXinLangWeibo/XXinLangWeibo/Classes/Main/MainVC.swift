f6f050a35fe6d43c75c6fa831d178f53be5c402b
xupengxiang <xupengxiang@lidehang.com>
2017-03-30 18:05:19 +0800

    方法的监听

--- XXinLangWeibo/XXinLangWeibo/Classes/Main/MainVC.swift
+++ XXinLangWeibo/XXinLangWeibo/Classes/Main/MainVC.swift
@@ -38,10 +38,16 @@ extension MainVC {
         
         // 2.设置属性
         
-        
         // 3.位置
         composeBtn.center = CGPoint(x: tabBar.center.x, y: tabBar.bounds.size.height*0.5)
+        
+        // 4监听点击事件
+        // Selector两种写法: 1> #selector(composeClick) 2> "composeClick"
+        composeBtn.addTarget(self, action: #selector(composeClick), for: .touchUpInside)
     }
+    
+    
+    
     
     /*
      func setupItemImages() {
@@ -63,5 +69,21 @@ extension MainVC {
     }
  */
     
+}
+
+
+// MARK: - 事件监听
+extension MainVC {
+    // 事件监听本质是发送消息,但是发送消息是OC的特性
+    // 将方法包装成@SEL --> 类中查找方法列表 --> 根据@SEL找到imp指针(函数指针) --> 执行函数
+    // swift中如果将一个函数声明为private, 那么该函数不会被添加到放大列表中
+    // 如果声明为private 程序执行时会找不到方法,导致崩溃
+    // 如果private前加上@objc该方法依然会被添加到方法列表中
+//    @objc private
+    func composeClick() {
+        
+        
+        
+    }
 }
 
