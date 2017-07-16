b1bac64e03db0a90f871125d35f5d27b662ccab8
许墨 <zztixupx@gmail.com>
2017-07-16 17:14:16 +0800

    nothing

--- XXinLangWeibo/XXinLangWeibo/Classes/Main/Welcome/WelcomeVC.swift
+++ XXinLangWeibo/XXinLangWeibo/Classes/Main/Welcome/WelcomeVC.swift
@@ -29,13 +29,12 @@ class WelcomeVC: UIViewController {
         // 2. 执行动画
         // Damping: 阻尼系数,主力系数越大,弹动的效果越不明显 0-1
         // initialSpringVelocity: 初始化速度
-        UIView.animate(withDuration: 1.5, delay: 0.0, usingSpringWithDamping: 0.7, initialSpringVelocity: 5.0, options: [], animations: {
+        UIView.animate(withDuration: 5.5, delay: 0.0, usingSpringWithDamping: 0.17, initialSpringVelocity: 3.0, options:[], animations: {
             
             self.view.layoutIfNeeded()
         }) { (_) in
             
             UIApplication.shared.keyWindow?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()
         }
-        
     }
 }
